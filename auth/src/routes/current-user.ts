1cc3a2d5d0e70153b277904157f0b19d06a6ed7f
vinaykum <vinaykum@adobe.com>
2021-01-23 21:11:46 +0530

    add missing test files

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@memory_map/common';
 const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, (req, res) => {
