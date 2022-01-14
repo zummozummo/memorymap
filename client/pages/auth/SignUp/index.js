2912b468a568ddf917dca95d0cf0e54cc1bce89f
anjali.goyal-at-337920175745 <anjali.goyal@incedoinc.com>
2022-01-14 16:13:08 +0530

    login popup commit

--- client/pages/auth/SignUp/index.js
+++ client/pages/auth/SignUp/index.js
@@ -9,15 +9,17 @@ class Signup extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
+			username: '',
 			email: '',
 			password: '',
-			name: ''
+			showPwd: false
 		}
 	}
 
 	onSubmit = () => {
 		event.preventDefault()
-		axios.post('/api/users/signin', {
+		axios.post('/api/users/signup', {
+			username: this.state.username,
 			email: this.state.email,
 			password: this.state.password
 		  })
@@ -36,26 +38,72 @@ class Signup extends React.Component {
 			[event.target.name]: event.target.value})
 	}
 
+	togglePwd = () => {
+		this.setState((prevState) => ({
+			showPwd: !prevState.showPwd
+		  }));
+	}
+
 	render() {
-		const { email, password, name } = this.state
+		const { email, password, username, showPwd } = this.state
 		return (
 			<div className={classes.container}>
-				<div className={classes.leftSignup}>
+				{/* <div className={classes.leftSignup}>
 					<img className={classes.landingImage} src='/images/home-image.jpg'/>
-				</div>
+				</div> */}
 				<div className={classes.signup}>
 				  <div className={classes.heading} > <h2 className={classes.text}>Welcome to Memory Map.</h2> </div>
 					{/* <div className={classes.heading} > <h2>Already signed up ? Log in</h2> </div> */}
+						{/* <div className={classes.buttonField}> 
+								<button
+								className={classes.button}
+								type="submit"
+								fullWidth
+								variant="contained"
+								color="primary"
+								// onClick={handleGoogleLogin}
+								>
+								<img
+									width="20px"
+									style={{ marginBottom: "0px", marginRight: "7px" }}
+									alt="Google sign-in"
+									src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
+								/>
+								Login with Google
+								</button>
+							</div>
+							<div className={classes.heading}><span className={classes.text} >OR</span>
+						</div> */}
 						<form onSubmit={this.onSubmit} >
 						<div className={classes.field} >
-								<input className={classes.inputField} placeholder="john" type="name" name="name" id="name" onChange={this.handleInputChange} value={name} />
-								<label className={classes.labelField} for="name">Username</label>
-							</div >							<div className={classes.field} >
+								<input className={classes.inputField} placeholder="john" type="name" name="username" id="username" onChange={this.handleInputChange} value={name} />
+								<label className={classes.labelField} for="username">Username</label>
+							</div >							
+							<div className={classes.field} >
 								<input className={classes.inputField} placeholder="johndoe@example.com" type="email" name="email" id="email" onChange={this.handleInputChange} value={email} />
 								<label className={classes.labelField} for="email">Email</label>
 							</div >
 							<div className={classes.field} >
-								<input className={classes.inputField} placeholder="j@*_7)(3/." type="password" name="password" id="password" onChange={this.handleInputChange} value={password} />
+								
+							{showPwd ? 
+								<img
+								onClick={this.togglePwd}
+								className={classes.eyeIcon}
+								width="20px"
+								style={{ marginBottom: "0px", marginRight: "7px" }}
+								alt="hideEyeIcon"
+								src='/images/hideEyeIcon.png'
+								/>:
+								<img
+									onClick={this.togglePwd}
+									className={classes.eyeIcon}
+									width="20px"
+									style={{ marginBottom: "0px", marginRight: "7px" }}
+									alt="eyeIcon"
+									src='/images/eyeIcon.png'
+								/>}
+								
+								<input className={classes.inputField} placeholder="j@*_7)(3/." type={showPwd ? "text" : "password"} name="password" id="password" onChange={this.handleInputChange} value={password} />
 								<label className={classes.labelField} for="password">Password</label>
 							</div >
 							<div className={classes.buttonField}> <button  className={classes.button} type="submit" > JOIN US </button></div >
