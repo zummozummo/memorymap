3349d3bb11852414fdba85f214e1689c567c3e93
anjali goyal <goyalanjali12@gmail.com>
2022-01-18 23:28:06 +0530

    signin api inegrated

--- client/pages/auth/SignIn/index.js
+++ client/pages/auth/SignIn/index.js
@@ -1,6 +1,9 @@
 import React from "react"
 import classes from './SignIn.module.css';
 import '../../../assets/home-image.jpg';
+const axios = require('axios');
+import useRequest from "../../../hooks/useRequest";
+import Router from 'next/router';
 
 class Signin extends React.Component {
 
@@ -8,58 +11,66 @@ class Signin extends React.Component {
 		super(props)
 		this.state = {
 			email: '',
-			password: '',
+			password: 'Qwerty@123!@#',
 			confirmPassword: '',
-			showPwd: false
+			showPwd: false,
+			errors: []
 		}
 	}
 
-	onSubmit = (event) => {
+
+	onSubmit = async (event) => {
 		event.preventDefault()
-            axios.post('/api/users/signin', {
-                email: this.state.email,
-                password: this.state.password
-              })
-              .then(function (response) {
-                console.log(response);
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
+
+		const response = await (useRequest({
+			url: '/api/users/signin',
+			method: 'post',
+			body: {
+				email: this.state.email,
+				password: this.state.password,
+			}
+		}))()
+		if (response.errors) {
+			this.setState({ errors: response.errors })
+		} else {
+			console.log(response)
+			Router.push('/getting-started')
+		}
 	}
 
 	handleInputChange = (event) => {
 		console.log(event.target.value, event.target.name);
 		this.setState({
 			...this.state,
-			[event.target.name]: event.target.value})
+			[event.target.name]: event.target.value
+		})
 	}
 
 	togglePwd = () => {
 		this.setState((prevState) => ({
 			showPwd: !prevState.showPwd
-		  }));
+		}));
 	}
 
 	render() {
-		const { email, password, showPwd } = this.state
+		const { email, password, showPwd, errors } = this.state
 		return (
 			<div className={classes.container}>
 				{/* <div className={classes.leftSignup}>
 					<img className={classes.landingImage} src='/images/home-image.jpg'/>
 				</div> */}
 				<div className={classes.signup}>
-				  <div className={classes.heading} > <h2 className={classes.text}>Welcome to Memory Map.</h2> </div>
+					<div className={classes.heading} > <h2 className={classes.text}>Welcome to Memory Map.</h2> </div>
 					{/* <div className={classes.heading} > <h2>Already signed up ? Log in</h2> </div> */}
-						<div className={classes.buttonField}> 
-							<button
+					<div className={classes.buttonField}>
+						<button
 							className={classes.button}
 							type="submit"
 							fullWidth
 							variant="contained"
 							color="primary"
-							// onClick={handleGoogleLogin}
-							>
+						// onClick={handleGoogleLogin}
+						>
 							<img
 								width="20px"
 								style={{ marginBottom: "0px", marginRight: "7px" }}
@@ -67,25 +78,25 @@ class Signin extends React.Component {
 								src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
 							/>
 							Login with Google
-							</button>
-						</div>
-						<div className={classes.heading}><span className={classes.text} >OR</span></div>
-						<form onSubmit={this.onSubmit} >
-							<div className={classes.field} >
-								<input className={classes.inputField} placeholder="johndoe@example.com" type="email" name="email" id="email" onChange={this.handleInputChange} value={email} />
-								<label className={classes.labelField} htmlFor="email">Email</label>
-							</div >
-							<div className={classes.field} >
-								
-								{showPwd ? 
+						</button>
+					</div>
+					<div className={classes.heading}><span className={classes.text} >OR</span></div>
+					<form onSubmit={this.onSubmit} >
+						<div className={classes.field} >
+							<input className={classes.inputField} placeholder="johndoe@example.com" type="email" name="email" id="email" onChange={this.handleInputChange} value={email} />
+							<label className={classes.labelField} htmlFor="email">Email</label>
+						</div >
+						<div className={classes.field} >
+
+							{showPwd ?
 								<img
-								onClick={this.togglePwd}
-								className={classes.eyeIcon}
-								width="20px"
-								style={{ marginBottom: "0px", marginRight: "7px" }}
-								alt="hideEyeIcon"
-								src='/images/hideEyeIcon.png'
-								/>:
+									onClick={this.togglePwd}
+									className={classes.eyeIcon}
+									width="20px"
+									style={{ marginBottom: "0px", marginRight: "7px" }}
+									alt="hideEyeIcon"
+									src='/images/hideEyeIcon.png'
+								/> :
 								<img
 									onClick={this.togglePwd}
 									className={classes.eyeIcon}
@@ -94,17 +105,22 @@ class Signin extends React.Component {
 									alt="eyeIcon"
 									src='/images/eyeIcon.png'
 								/>}
-								
-								<input className={classes.inputField} placeholder="j@*_7)(3/." type={showPwd ? "text" : "password"} name="password" id="password" onChange={this.handleInputChange} value={password} />
-								<label className={classes.labelField} for="password">Password</label>
-								
-							</div >
-							<div className={classes.buttonField}> <button  className={classes.button} type="submit" > LOGIN </button></div >
-						</form>
+
+							<input className={classes.inputField} placeholder="j@*_7)(3/." type={showPwd ? "text" : "password"} name="password" id="password" onChange={this.handleInputChange} value={password} />
+							<label className={classes.labelField} for="password">Password</label>
+
+						</div >
+
+						{errors.length > 0 && <div>
+							<h4>Ooops...</h4>
+							<ul>{errors.map(err => <li key={err.message}>{err.message}</li>)}</ul>
+						</div>}
+						<div className={classes.buttonField}> <button className={classes.button} type="submit" > LOGIN </button></div >
+					</form>
 				</div >
 			</div>
 		)
- }
+	}
 }
 
 
