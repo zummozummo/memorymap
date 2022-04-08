b32295368319b3b555b04c5758767380ceef8b3f
Vinay Kumar <vshakya655@gmail.com>
2022-04-08 18:56:22 +0530

    added auth call in getServersideprops

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,29 +1,36 @@
-import Head from 'next/head';
-import { Fragment } from 'react';
-
-function HomePage(props) {
-    return (
-        <Fragment>
-        <Head>
-          <title>Memory Map</title>
-          <meta
-            name='description'
-            content='Map your memory'
-          />
-        </Head>
-      </Fragment>
-        
-    )
+import Head from "next/head";
+import { Fragment } from "react";
+import axios from "axios";
+function HomePage({ user }) {
+  console.log(user);
+  return (
+    <Fragment>
+      <Head>
+        <title>Memory Map</title>
+        <meta name="description" content="Map your memory" />
+      </Head>
+      <>body</>
+    </Fragment>
+  );
 }
 
-
-export function getStaticProps() {
-  
-    return {
-      props: {
-      },
-    };
+export async function getServerSideProps({ req }) {
+  let user = "";
+  if (typeof window === "undefined") {
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+    user = data;
   }
-  
-  export default HomePage;
-  
+  console.log(user);
+  return {
+    props: {
+      user,
+    },
+  };
+}
+
+export default HomePage;
